db00929214f3b42352d1525a18cd85e1fbeb054d
7minutech <elijohnson1107@gmail.com>
2024-08-15 17:45:37 -0500

    Changed nodes to coords
    Will make nodes from coords

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,16 +1,19 @@
 class Graph
-  attr_accessor :nodes
+  attr_accessor :coords
 
   def initialize
-    @nodes = []
-    build_nodes
+    @coords = []
+    build_coords
   end
 
-  def build_nodes
+  def build_coords
     (0..7).each do |x|
       (0..7).each do |y|
-        @nodes.push([x, y])
+        @coords.push([x, y])
       end
     end
+  end
+
+  def build_graph
   end
 end
