55912db4b6bad7dd58b9126709880c7651200829
7minutech <elijohnson1107@gmail.com>
2024-08-15 17:42:34 -0500

    Added attr accessor for graph
    Need to access nodes for debugging

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,12 +1,15 @@
 class Graph
+  attr_accessor :nodes
+
   def initialize
     @nodes = []
+    build_nodes
   end
 
   def build_nodes
     (0..7).each do |x|
       (0..7).each do |y|
-        nodes.push([x, y])
+        @nodes.push([x, y])
       end
     end
   end
